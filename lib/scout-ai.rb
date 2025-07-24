1b996f4113f0597d01f325a9db0509a446964276
Miguel Vazquez <mikisvaz@gmail.com>
2025-07-24 14:22:45 +0200

    Add require to scout-ai

--- lib/scout-ai.rb
+++ lib/scout-ai.rb
@@ -5,5 +5,6 @@ require 'scout/resource'
 Path.add_path :scout_ai_lib, File.join(Path.caller_lib_dir(__FILE__), "{TOPLEVEL}/{SUBPATH}")
 
 require 'scout/llm/ask'
+require 'scout/llm/chat'
 require 'scout/llm/embed'
 require 'scout/llm/agent'
